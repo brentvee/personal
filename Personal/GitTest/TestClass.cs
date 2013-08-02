ac2abc0e763eb91da06e1877771fd01c920b6206
Brent van der Merwe <brent@inivit.com>
2013-08-02 11:54:36 +0200

    my first commit

---
+++ Personal/GitTest/TestClass.cs
@@ -0,0 +1,27 @@
+﻿/*¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯*\
+  File Name: TestClass.cs
+  Language:  C#
+  Platform:  [x] .net
+             [ ] Silverlight
+  Company:   Inivit Systems
+             This code and information is provided “as is” without warranty of 
+             any kind, either expressed or implied.
+  Author:    Brent van der Merwe
+  Date:      2013-08-02 11:48 
+  Purpose:	
+    
+  Updates:
+
+	Reviews
+\*____________________________________________________________________________*/
+
+namespace GitTest
+{
+    public class TestClass
+    {
+        public void Method1()
+        {
+
+        }
+    }
+}
